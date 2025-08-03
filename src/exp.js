d0887c2b4de0f9f043557e4ad5d8aa55f941149a
Harrison Tran <harrit4@uci.edu>
2025-08-03 15:15:31 -0700

    experience page template

--- src/exp.js
+++ src/exp.js
@@ -1,1 +1,46 @@
+import * as TWEEN from '@tweenjs/tween.js';
 import "./style.css"
+
+import * as THREE from 'three';
+import { PointLight } from "three";
+
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+import { getFresnelMat } from './getFresnelMat.js';
+
+import getStarfield from './getStarfield.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+renderer.render( scene, camera );
+
+// const backgroundTexture = new THREE.TextureLoader().load('A$.png');
+// scene.background = backgroundTexture;
+
+//resize the canvas to fit the window
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize );
+
+const starfield = getStarfield();
+scene.add(starfield);
+
+function animate(){
+    requestAnimationFrame( animate );
+    renderer.render(scene, camera);
+}
+
+animate();
